8877709ba2efb7dff67808cbd796785b188ed7f1
Vokler <valerypavlikov@yandex.ru>
2021-02-25 21:16:06 +0300

    created Product and ProductCategory

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=True)
+    short_description = models.CharField(max_length=64, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images', blank=True)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
