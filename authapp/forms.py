3fa92894ca62abe8d18f07a6d93fa4f50e1ce7a1
Vokler <valerypavlikov@yandex.ru>
2021-03-01 22:12:38 +0300

    added login

---
+++ authapp/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from authapp.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Введите имя пользователя'
+        self.fields['password'].widget.attrs['placeholder'] = 'Введите пароль'
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
--- authapp/views.py
+++ authapp/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+from django.contrib import messages
+
+from authapp.forms import UserLoginForm, UserCreationForm
 
 
 def login(request):
-    return render(request, 'authapp/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'authapp/login.html', context)
 
 
 def register(request):
