0539353c4afea81a5b829c757b2e959d496d0953
Vokler <valerypavlikov@yandex.ru>
2021-03-04 21:28:27 +0300

    added logic for basket template

--- authapp/views.py
+++ authapp/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.contrib import messages
 
 from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm
+from basketapp.models import Basket
 
 
 def login(request):
@@ -45,7 +46,10 @@ def profile(request):
             return HttpResponseRedirect(reverse('auth:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'form': form}
+    context = {
+        'form': form,
+        'baskets': Basket.objects.all(),
+    }
     return render(request, 'authapp/profile.html', context)
 
 
