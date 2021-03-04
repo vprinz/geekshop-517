124473d7bfd7012317bcf91152eef28424451a15
Vokler <valerypavlikov@yandex.ru>
2021-03-04 21:52:57 +0300

    added basket-delete controller

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from basketapp.views import basket_add
+from basketapp.views import basket_add, basket_delete
 
-app_name = 'authapp'
+app_name = 'basketapp'
 
 urlpatterns = [
     path('basket-add/<int:product_id>/', basket_add, name='basket_add'),
+    path('basket-delete/<int:id>/', basket_delete, name='basket_delete'),
 ]
--- basketapp/views.py
+++ basketapp/views.py
@@ -19,3 +19,9 @@ def basket_add(request, product_id=None):
         basket.quantity += 1
         basket.save()
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def basket_delete(request, id=None):
+    basket = Basket.objects.get(id=id)
+    basket.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
