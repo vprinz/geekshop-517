83ddd373f35ed0153bf127b4076b915f30231151
Vokler <valerypavlikov@yandex.ru>
2021-03-15 21:13:27 +0300

    added Pagination

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,5 +6,6 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', products, name='index'),
-    path('<int:id>/', products, name='product'),
+    path('<int:category_id>/', products, name='product'),
+    path('page/<int:page>/', products, name='page')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,7 @@
-import os
-import json
-
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from mainapp.models import Product, ProductCategory
-
-dir = os.path.dirname(__file__)
 
 
 # функции = вьюхи = контроллеры
@@ -14,10 +10,13 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request, id=None):
-    context = {
-        'title': 'GeekShop - Каталог',
-        'products': Product.objects.all(),
-        'categories': ProductCategory.objects.all(),
-    }
+def products(request, category_id=None, page=1):
+    context = {'title': 'GeekShop - Каталог', 'categories': ProductCategory.objects.all()}
+    if category_id:
+        products = Product.objects.filter(category_id=category_id).order_by('-price')
+    else:
+        products = Product.objects.all().order_by('-price')
+    paginator = Paginator(products, 3)
+    products_paginator = paginator.page(page)
+    context.update({'products': products_paginator})
     return render(request, 'mainapp/products.html', context)
